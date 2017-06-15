362308613d0363b00457d97b3af6a75b96a40006
Xiardon <marcos.av@outlook.es>
2017-06-15 02:31:48 +0200

    Primera version funcional y jugable

--- Juego.java
+++ Juego.java
@@ -6,6 +6,7 @@ import javafx.scene.shape.*;
 import javafx.animation.*;
 import javafx.scene.control.*;
 import javafx.scene.text.*;
+import javafx.scene.paint.*;
 /**
  * Esta es la clase principal que se encargara de lanzar el juego y crear el campo.
  * 
@@ -25,7 +26,9 @@ public class Juego extends Application
     private static Jugador jugador2;
     private Label marcadorJugador1;
     private Label marcadorJugador2;
-    
+    private Label ganador; //Es el texto que aparecera en pantalla si hay un ganador.
+    private static Stage escenario; //Nuestra ventana de aplicacion
+    AnimationTimer animacion; //Representa el moviento y los metodos que ejecutaran dentro de ella los elementos.
 
     /**
      * Constructor de nuestra clase
@@ -36,7 +39,6 @@ public class Juego extends Application
         numeroJugadores = 0;
 
     }
-
     public static void main(String[] args){
         launch(args);
     }
@@ -46,41 +48,43 @@ public class Juego extends Application
      */
     @Override
     public void start(Stage escenario){
+        this.escenario = escenario;
         crearCampo();
         crearJugadores();
         crearMarcadores();
         escenario.setTitle("EL CLASICO PONG HECHO POR MARCOS"); //Ponemos titulo a la ventana.
         escenario.setScene(escena); //Introducimos la escena en el escenario.
         escenario.show(); //Lo mostramos
-        AnimationTimer animacion = new AnimationTimer(){
-                @Override
-                public void handle(long now){
-                    BOLA.mover();
-                    jugador1.mover();
-                    jugador2.mover();
-                    actualizarMarcadores();
-                }
-            };
-        animacion.start();
-        
-        escena.setOnKeyPressed(event -> {
-            switch(event.getCode()){
-                case UP:
-                jugador1.moverArriba();
-                break;
-                
-                case DOWN:
-                jugador1.moverAbajo();
-                break;
-                
-                case W:
-                jugador2.moverArriba();
-                break;
-                
-                case S:
-                jugador2.moverAbajo();
-                break;
+        animacion = new AnimationTimer(){
+            @Override
+            public void handle(long now){
+                BOLA.mover();
+                jugador1.mover();
+                jugador2.mover();
+                actualizarMarcadores();
+                comprobarSiHayGanador();
             }
+        };
+        animacion.start();
+
+        escena.setOnKeyPressed(event -> {
+                switch(event.getCode()){
+                    case UP:
+                    jugador1.moverArriba();
+                    break;
+
+                    case DOWN:
+                    jugador1.moverAbajo();
+                    break;
+
+                    case W:
+                    jugador2.moverArriba();
+                    break;
+
+                    case S:
+                    jugador2.moverAbajo();
+                    break;
+                }
             });
 
     }
@@ -93,7 +97,16 @@ public class Juego extends Application
         escena.setFill(Color.SPRINGGREEN); //El fondo de la escena servira como campo verde.
         Rectangle lineaCentral = new Rectangle(3, ALTURA_CAMPO, Color.WHITE);
         lineaCentral.setTranslateX(ANCHURA_CAMPO / 2);
-        grupo.getChildren().addAll(lineaCentral, BOLA);
+
+        //El texto para el ganador.
+        ganador = new Label("prueba");
+        ganador.layoutXProperty().bind(escenario.widthProperty().subtract(ganador.widthProperty()).divide(2));
+        ganador.setTranslateY(150);
+        ganador.setFont(Font.font(30));
+        ganador.setTextFill(Color.DARKORANGE);
+        ganador.setVisible(false);
+
+        grupo.getChildren().addAll(ganador, lineaCentral, BOLA);
     }
 
     /**
@@ -135,7 +148,7 @@ public class Juego extends Application
     public static int getNumeroJugadores(){
         return numeroJugadores;
     }
-    
+
     /**
      * Metodo que crea los marcadores y los añade al campo.
      */
@@ -145,21 +158,36 @@ public class Juego extends Application
         marcadorJugador1.setTranslateY(10);
         marcadorJugador1.setTextFill(jugador1.getColor());
         marcadorJugador1.setFont(Font.font(15));
-        
+
         marcadorJugador2 = new Label("JUGADOR 2   " + String.valueOf(jugador2.getPuntuacion()));
         marcadorJugador2.setTranslateX(Juego.getAnchuraCampo() - (Juego.getAnchuraCampo() / 3));
         marcadorJugador2.setTranslateY(10);
         marcadorJugador2.setTextFill(jugador2.getColor());
         marcadorJugador2.setFont(Font.font(15));
         grupo.getChildren().addAll(marcadorJugador1, marcadorJugador2);
-        
+
     }
-    
+
     /**
      * Metodo que actualiza el resultado mostrado en los marcadores.
      */
     private void actualizarMarcadores(){
-        marcadorJugador1.setText("JUGADOR 1   " + String.valueOf(jugador1.getPuntuacion()));
-        marcadorJugador2.setText("JUGADOR 2   " + String.valueOf(jugador2.getPuntuacion()));
+        marcadorJugador1.setText("JUGADOR 1   " + String.valueOf(jugador1.getPuntuacion()) + " pts");
+        marcadorJugador2.setText("JUGADOR 2   " + String.valueOf(jugador2.getPuntuacion()) + " pts");
+    }
+
+    /**
+     * Este metodo comprueba que si hay un ganador y lo muestra por pantalla.
+     */
+    private void comprobarSiHayGanador(){
+        if(jugador1.getPuntuacion() == 10){
+            ganador.setText("El JUGADOR 1 ha ganado!!!");
+            ganador.setVisible(true);
+            animacion.stop();
+        }else if(jugador2.getPuntuacion() == 10){
+            ganador.setText("El JUGADOR 2 ha ganado!!!");
+            ganador.setVisible(true);
+            animacion.stop();
+        }
     }
 }
